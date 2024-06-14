cf1590f965b9a2f02c5e6fbcb6dce47506b4a2b3
jose-izarra <jaiza0912@gmail.com>
2024-06-14 12:50:41 +0200

    made a bit prettier

--- app/app/components/whisper.tsx
+++ app/app/components/whisper.tsx
@@ -26,22 +26,24 @@ export default function Whisper() {
 
 
     return (
-        <div className='h-screen flex flex-col items-center justify-end px-5'>
+        <div className='h-screen flex flex-col items-center justify-center px-16'>
 
         {
             transcripts.length > 0 ? (
                 <>
-                    <h1 className="text-2xl font-bold">Whisper</h1>
-                    <p className='font-sans text-lg'>{transcripts}</p>
+                    <h1 className="text-2xl font-bold pb-5">Whisper</h1>
+                    <p className='font-sans text-lg '>{transcripts}</p>
                      
                 </>
             ) : (
-                <h1 className="text-2xl font-bold">No Whisper</h1>
+                <>
+                    <h1 className="text-2xl font-bold pb-5">No Whisper</h1>
+                    <p className='font-sans text-lg max-w-[60%]'>Please start the flask server !</p>
+                </>                
             )
             
-
         }
-            
+            <p className='pt-16'><a target='_blank' href='https://github.com/IsmaPi/Whisper-real-time-transcription' className='link link-primary'>Link to github</a></p>
         </div>
     )
 }
